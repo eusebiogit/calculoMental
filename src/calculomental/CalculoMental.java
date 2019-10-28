7c856df8990278264407f757ec712e8a30864d4f
Eusebio <eusebio.guijarro@protonmail.com>
2019-10-28 14:02:03 +0100

    initial commit

---
+++ src/calculomental/CalculoMental.java
@@ -0,0 +1,18 @@
+package calculomental;
+
+/**
+ *
+ * @author ordenador
+ */
+public class CalculoMental {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(2);
+
+
+    }
+    
+}
